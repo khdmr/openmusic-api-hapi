7a5af2e9ccfba0481f33d6acf5499ebcfd1e6b1d
khdmr <khairul.damar@gmail.com>
2024-05-29 22:54:20 +0700

    feat(songs): add insert service for songs

---
+++ src/services/postgres/SongsService.js
@@ -0,0 +1,27 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class SongsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addSongs(title, year, genre, performer, duration, albumId) {
+    const songId = `song-${nanoid(16)}`;
+    const query = {
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+      values: [songId, title, year, genre, performer, duration, albumId],
+    };
+
+    const result = this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+}
+
+module.exports = SongsService;
