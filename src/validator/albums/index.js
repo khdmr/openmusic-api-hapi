742d7b1a0bd82e0ab0fbcdbc6430c5a6d276576c
khdmr <khairul.damar@gmail.com>
2024-05-29 20:43:46 +0700

    feat(albums): add validation to validate album payload

---
+++ src/validator/albums/index.js
@@ -0,0 +1,12 @@
+const AlbumPayloadSchema = require('./schema');
+
+const AlbumsValidator = {
+  validateAlbumPayload: (payload) => {
+    const validationResult = AlbumPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new Error(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = AlbumsValidator;
