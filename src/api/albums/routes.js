45db615585697e88df229ddb609f1e4dbb34e944
khdmr <khairul.damar@gmail.com>
2024-05-29 22:19:44 +0700

    feat(albums): add delete route for albums

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -14,6 +14,11 @@ const routes = (handler) => [
     path: '/albums/{id}',
     handler: handler.putAlbumByIdHandler,
   },
+  {
+    method: 'DELETE',
+    path: '/albums/{id}',
+    handler: handler.deleteAlbumByIdHandler,
+  },
 ];
 
 module.exports = routes;
