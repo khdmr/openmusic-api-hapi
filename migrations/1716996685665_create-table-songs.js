3df712e6e3e043c3dbda04f3d723b7fc8e3fb9f9
khdmr <khairul.damar@gmail.com>
2024-05-29 22:38:53 +0700

    feat(songs): add migrations for songs

---
+++ migrations/1716996685665_create-table-songs.js
@@ -0,0 +1,59 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+      notNull: false,
+    },
+    albumId: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+    },
+  });
+
+  pgm.addConstraint('songs', 'fk_songs.albumId_albums.id', {
+    foreignKeys: {
+      columns: 'albumId',
+      references: 'albums(id)',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropConstraint('songs', 'fk_songs.albumId_albums.id');
+  pgm.dropTable('songs');
+};
