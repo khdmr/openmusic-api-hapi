4931075d90cc1cc341bd62254d2c3ab456322f8b
khdmr <khairul.damar@gmail.com>
2024-05-29 21:19:38 +0700

    feat(albums): add service to get album by id from database

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -2,6 +2,7 @@ const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 
 const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class AlbumsService {
   constructor() {
@@ -23,6 +24,21 @@ class AlbumsService {
     }
 
     return result.rows[0].id;
+  }
+
+  async getAlbumById(albumId) {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [albumId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Album tidak ditemukan');
+    }
+
+    return result.rows[0];
   }
 }
 
