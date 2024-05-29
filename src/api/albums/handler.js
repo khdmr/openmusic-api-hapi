8b45fcebe7bea2d3b3cb78757c2eb406649af1fa
khdmr <khairul.damar@gmail.com>
2024-05-29 20:01:27 +0700

    feat(albums): add handler for post album

---
+++ src/api/albums/handler.js
@@ -0,0 +1,32 @@
+class AlbumsHandler {
+  constructor(service) {
+    this._service = service;
+  }
+
+  async postAlbumHandler(request, h) {
+    try {
+      const { name, year } = request.payload;
+
+      const albumId = await this._service.addAlbum(name, year);
+
+      const response = h.response({
+        status: 'success',
+        data: {
+          id: albumId,
+        },
+      });
+
+      response.code(201);
+      return response;
+    } catch (error) {
+      const response = h.response({
+        status: 'fail',
+        message: error.message,
+      });
+      response.code(400);
+      return response;
+    }
+  }
+}
+
+module.exports = AlbumsHandler;
