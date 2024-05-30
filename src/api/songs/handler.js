462515d746c50ab216c32fba7b2ed46099cb023d
khdmr <khairul.damar@gmail.com>
2024-05-30 12:42:47 +0700

    feat(songs): add get song by id handler for songs

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -5,6 +5,7 @@ class SongsHandler {
 
     this.postSongHandler = this.postSongHandler.bind(this);
     this.getSongsHandler = this.getSongsHandler.bind(this);
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
   }
 
   async postSongHandler(request, h) {
@@ -46,6 +47,22 @@ class SongsHandler {
       status: 'success',
       data: {
         songs,
+      },
+    });
+
+    response.code(200);
+    return response;
+  }
+
+  async getSongByIdHandler(request, h) {
+    const { id: songId } = request.params;
+
+    const song = await this._service.getSongById(songId);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        song,
       },
     });
 
