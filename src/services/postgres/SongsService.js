8a7b9533e4f3884ae9c1ec38b249972d169dab5a
khdmr <khairul.damar@gmail.com>
2024-05-30 15:58:27 +0700

    fix(songs): fix query in getSongs

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -24,8 +24,23 @@ class SongsService {
     return result.rows[0].id;
   }
 
-  async getSongs() {
-    const result = await this._pool.query('SELECT id, title, performer FROM songs');
+  async getSongs({ title, performer }) {
+    const query = {
+      text: `
+      SELECT 
+        id, 
+        title, 
+        performer 
+      FROM 
+        songs
+      WHERE
+        title ILIKE $1 
+      AND
+        performer ILIKE $2
+      `,
+      values: [`%${title}%`, `%${performer}%`],
+    };
+    const result = await this._pool.query(query);
 
     return result.rows;
   }
