259d04a11520a05aa152d7aba78ce482cc7a719d
khdmr <khairul.damar@gmail.com>
2024-05-31 14:14:54 +0700

    feat(users): add user credential verification

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 
 const InvariantError = require('../../exceptions/InvariantError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -13,7 +14,7 @@ class UsersService {
     await this.verifyNewUsername(username);
 
     const userId = `user-${nanoid(16)}`;
-    const hashedPassword = bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const query = {
       text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
@@ -37,6 +38,27 @@ class UsersService {
     if (result.rows.length > 0) {
       throw new InvariantError('User gagal ditambahkan. Username sudah digunakan');
     }
+  }
+
+  async verifyUserCredential({ username, password }) {
+    const query = {
+      text: 'SELECT * FROM users WHERE username = $1',
+      values: [username],
+    };
+    const result = await this._pool.query(query);
+    console.log(result.rows);
+    if (!result.rowCount) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+    console.log(hashedPassword)
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
   }
 }
 
