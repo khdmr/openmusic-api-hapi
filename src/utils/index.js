16752ad2737554693496c9a8a99ef5235a35d698
khdmr <khairul.damar@gmail.com>
2024-05-31 21:51:42 +0700

    feat: add function to convert output with specific criteria

--- src/utils/index.js
+++ src/utils/index.js
@@ -19,6 +19,28 @@ const mapDBAlbumToModel = (songs) => {
   return mapSong;
 };
 
+const mapDBPlaylistSongsToModel = (playlists) => {
+  const mapPlaylistSongs = {
+    id: playlists[0].id,
+    name: playlists[0].name,
+    username: playlists[0].username,
+    songs: [],
+  };
+
+  playlists.forEach((playlist) => {
+    if (playlist.id !== null) {
+      mapPlaylistSongs.songs.push({
+        id: playlist.song_id,
+        title: playlist.title,
+        performer: playlist.performer,
+      });
+    }
+  });
+
+  return mapPlaylistSongs;
+};
+
 module.exports = {
   mapDBAlbumToModel,
+  mapDBPlaylistSongsToModel,
 };
