30c82613df7fb82c1fb3f9129891d13a56251de7
khdmr <khairul.damar@gmail.com>
2024-05-31 19:14:31 +0700

    feat: add playlist_songs migrations

---
+++ migrations/1717156650128_create-table-playlist-songs.js
@@ -0,0 +1,54 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('playlist_songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('playlist_songs', 'unique_playlist_id_and_song_id', 'UNIQUE(playlist_id, song_id)');
+
+  pgm.addConstraint('playlist_songs', 'fk_playlist_songs.playlist_id_playlists.id', {
+    foreignKeys: {
+      columns: 'playlist_id',
+      references: 'playlists(id)',
+      onDelete: 'CASCADE',
+    },
+  });
+  pgm.addConstraint('playlist_songs', 'fk_playlist_songs.song_id_songs.id', {
+    foreignKeys: {
+      columns: 'song_id',
+      references: 'songs(id)',
+      onDelete: 'CASCADE',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropConstraint('playlist_songs', 'fk_playlist_songs.playlist_id_playlists.id');
+  pgm.dropConstraint('playlist_songs', 'fk_playlist_songs.song_id_songs.id');
+  pgm.dropTable('playlist_songs');
+};
