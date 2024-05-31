4c9f5b4da91bc8cc93e7d567a128dc54e7aa83af
khdmr <khairul.damar@gmail.com>
2024-05-31 14:17:35 +0700

    mfeat(auth): add validation schema

---
+++ src/validator/authentications/index.js
@@ -0,0 +1,14 @@
+const PostAuthenticationPayloadSchema = require('./schema');
+
+const InvariantError = require('../../exceptions/InvariantError');
+
+const AuthenticationsValidator = {
+  validatePostAuthenticationPayload: (payload) => {
+    const validationResult = PostAuthenticationPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = AuthenticationsValidator;
