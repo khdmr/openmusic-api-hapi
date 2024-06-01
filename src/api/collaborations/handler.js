2195088b89bd4618c2dc03d5c782046c4c22d502
khdmr <khairul.damar@gmail.com>
2024-06-01 18:10:16 +0700

    feat(collab): add API to delete collaboration

--- src/api/collaborations/handler.js
+++ src/api/collaborations/handler.js
@@ -6,6 +6,7 @@ class CollaborationsHandler {
     this._validator = validator;
 
     this.postCollaboratorHandler = this.postCollaboratorHandler.bind(this);
+    this.deleteCollaboratorHandler = this.deleteCollaboratorHandler.bind(this);
   }
 
   async postCollaboratorHandler(request, h) {
@@ -13,8 +14,8 @@ class CollaborationsHandler {
 
     const { userId } = request.auth.credentials;
     const { playlistId, userId: userIdCollab } = request.payload;
-    await this._usersService.verifyUserIsExist(userIdCollab);
 
+    await this._usersService.verifyUserIsExist(userIdCollab);
     await this._playlistService.verifyPlaylistOwner({ playlistId, userId });
 
     const collaborationId = await this._collaborationsService.addCollaboration({
@@ -29,6 +30,28 @@ class CollaborationsHandler {
       },
     });
     response.code(201);
+    return response;
+  }
+
+  async deleteCollaboratorHandler(request, h) {
+    this._validator.validateCollaborationPayload(request.payload);
+
+    const { userId } = request.auth.credentials;
+    const { playlistId, userId: userIdCollab } = request.payload;
+
+    await this._playlistService.verifyPlaylistOwner({ playlistId, userId });
+    await this._usersService.verifyUserIsExist(userIdCollab);
+
+    await this._collaborationsService.deleteCollaboration({
+      playlistId,
+      userIdCollab,
+    });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Kolaborasi berhasil dihapus',
+    });
+    response.code(200);
     return response;
   }
 }
