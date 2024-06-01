72afc419df2b39ed7bb53f27621faca5f1429f4a
khdmr <khairul.damar@gmail.com>
2024-06-01 17:38:36 +0700

    feat(playlists): inject collaborationsService to playlistsService (dependency injection)

--- src/server.js
+++ src/server.js
@@ -47,9 +47,9 @@ const init = async () => {
   const songsService = new SongsService();
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const playlistsService = new PlaylistsService();
   const activitiesService = new ActivitiesService();
   const collaborationsService = new CollaborationsService();
+  const playlistsService = new PlaylistsService(collaborationsService);
 
   const server = Hapi.server({
     port: process.env.PORT,
