7f2e03e87d73bd2b349e856251fd9fdb5a83701d
khdmr <khairul.damar@gmail.com>
2024-06-03 12:03:47 +0700

    feat(albums): add validator to validate header form uploaded file

--- src/validator/albums/index.js
+++ src/validator/albums/index.js
@@ -1,10 +1,20 @@
-const AlbumPayloadSchema = require('./schema');
+const {
+  AlbumPayloadSchema,
+  CoverImageHeadersSchema,
+} = require('./schema');
 
 const InvariantError = require('../../exceptions/InvariantError');
 
 const AlbumsValidator = {
   validateAlbumPayload: (payload) => {
     const validationResult = AlbumPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+
+  validateCoverImageHeaders: (hedaers) => {
+    const validationResult = CoverImageHeadersSchema.validate(hedaers);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
     }
